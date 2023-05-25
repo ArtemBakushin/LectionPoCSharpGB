09f7240282dc2899d1d8f52e401ce3f7a2009e77
artem <artem28282828@gmail.com>
2023-05-25 19:59:32 +0500

    Привет маша

---
+++ Example005_Collection/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя : ");
+string username = Console.ReadLine();
+if (username.ToLower() == "Masha")
+{
+    Console.WriteLine("Ура это же Masha");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
